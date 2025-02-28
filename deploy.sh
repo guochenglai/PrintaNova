20cf34f5aaa92f55562898da0683bda6d14a2337
Steven Guo <stevenguo@microsoft.com>
2025-02-28 11:38:11 +0800

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,15 @@
 #!/bin/bash
 
-# 服务器信息
 # Read from Windows environment variables with fallbacks
-SERVER=${DEPLOY_SERVER:-%DEPLOY_SERVER%}
-# If it's still the Windows variable format, use the default
-if [ "$SERVER" = "%DEPLOY_SERVER%" ]; then
-  SERVER="52.186.171.143"
-fi
-
-USER=${DEPLOY_USER:-%DEPLOY_USER%}
-if [ "$USER" = "%DEPLOY_USER%" ]; then
-  USER="azureuser_printing"
-fi
-
-PASSWORD=${DEPLOY_PASSWORD:-%DEPLOY_PASSWORD%}
-if [ "$PASSWORD" = "%DEPLOY_PASSWORD%" ]; then
-  PASSWORD="Ekua_1234567"
-fi
+SERVER=${DEPLOY_SERVER}
+USER=${DEPLOY_USER}
+PASSWORD=${DEPLOY_PASSWORD}
 
 DIRECTORY="/home/azureuser_printing/PrintaNova"
 LOGFILE="deploy.log"
 
-# 开始日志记录
 echo "Deployment started at $(date)" > $LOGFILE
 
-# 使用SSH登录并执行命令
 ssh $USER@$SERVER << EOF | tee -a $LOGFILE
   echo "Logged into $SERVER"
   cd $DIRECTORY
@@ -40,5 +25,4 @@ ssh $USER@$SERVER << EOF | tee -a $LOGFILE
   echo "Executed sudo npm restart"
 EOF
 
-# 结束日志记录
 echo "Deployment finished at $(date)" >> $LOGFILE
